ecac9e2c14ca363939b232e111d913c79fccb5a1
Adam Docherty <uklooneyjr@gmail.com>
2018-01-09 12:14:32 +0000

    Improved regexes for targets and errors somewhat.

--- antcolour.ps1
+++ antcolour.ps1
@@ -15,9 +15,9 @@ function Ant-Colour {
       "Buildfile:" { $consoleWriteColour = "Gray"; break }
       "BUILD SUCCESSFUL" { $consoleWriteColour = "Green"; break }
       "BUILD FAILED" { $consoleWriteColour = "Red"; break }
-      "^[^\s]*:" { $consoleWriteColour = "Cyan"; break }
-      "[Ee](rror|RROR):" { $consoleWriteColour = "Red"; break }
-      default { $consoleWriteColour = (Get-Host).ui.rawui.ForegroundColor; break }
+      "^[^\s]*:\s$" { $consoleWriteColour = "Cyan"; break }
+      "[Ee](rror|RROR)" { $consoleWriteColour = "Red"; break }
+      default { $consoleWriteColour = (Get-Host).ui.rawui.ForegroundColor }
     }
     Write-Host $contentLine -ForegroundColor $consoleWriteColour -NoNewline
   }
